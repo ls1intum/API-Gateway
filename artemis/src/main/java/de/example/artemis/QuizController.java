8c6616c558a92ea5b7717f080ec0ed2262a6f6eb
Ole Vester <ole.vester+gitlab@check24.de>
2025-01-05 18:18:45 +0100

    load and response with instance name on QuizController and TextController

--- artemis/src/main/java/de/example/artemis/QuizController.java
+++ artemis/src/main/java/de/example/artemis/QuizController.java
@@ -1,5 +1,6 @@
 package de.example.artemis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/")
 public class QuizController {
 
+    @Value("${artemis.instance-name}")
+    private String instanceName;
+
     @GetMapping("/quiz")
     public String getText() {
-        return "Quiz enabled";
+        return "Quiz enabled for instanceName " + instanceName;
     }
 }
--- artemis/src/main/java/de/example/artemis/TextController.java
+++ artemis/src/main/java/de/example/artemis/TextController.java
@@ -1,5 +1,6 @@
 package de.example.artemis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/")
 public class TextController {
 
+    @Value("${artemis.instance-name}")
+    private String instanceName;
+
     @GetMapping("/text")
     public String getText() {
-        return "Text enabled";
+        return "Text enabled for instanceName " + instanceName;
     }
 }
