655580af388aec91ab8efc8aee948eb83870e438
Ole Vester <ole.vester+gitlab@check24.de>
2025-01-06 11:18:07 +0100

    use Environment instead of env var to look up spring profiles

--- artemis/src/main/java/de/example/artemis/ServiceDiscoveryConfig.java
+++ artemis/src/main/java/de/example/artemis/ServiceDiscoveryConfig.java
@@ -3,25 +3,29 @@ package de.example.artemis;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class ServiceDiscoveryConfig {
 
-    @Value("${spring.profiles.active}")
-    private String activeProfiles;
+    private final Environment environment;
 
     private final EurekaInstanceConfigBean eurekaInstanceConfig;
 
-    public ServiceDiscoveryConfig(EurekaInstanceConfigBean eurekaInstanceConfig) {
+    public ServiceDiscoveryConfig(Environment environment, EurekaInstanceConfigBean eurekaInstanceConfig) {
+        this.environment = environment;
         this.eurekaInstanceConfig = eurekaInstanceConfig;
     }
 
     @PostConstruct
     public void setProfileMetadata() {
-        if (activeProfiles.isEmpty()) {
+        var activeProfiles = environment.getActiveProfiles();
+        if (activeProfiles.length == 0) {
             throw new IllegalArgumentException("No active profiles set. Required for service discovery.");
         }
-        eurekaInstanceConfig.getMetadataMap().put("profile", activeProfiles);
+
+        eurekaInstanceConfig.getMetadataMap().put("profile", String.join(",", activeProfiles));
     }
 }
