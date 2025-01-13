e4225c67cd8f2dcf153a46851520bf79c5c8bb27
Ole Vester <ole.vester+gitlab@check24.de>
2025-01-13 15:42:38 +0100

    rename Spring Boot starter class

--- api-gateway/src/main/java/de/example/gateway/ArtemisAPIGateway.java
+++ api-gateway/src/main/java/de/example/gateway/ArtemisAPIGateway.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultiplServicesConsulApplication {
+public class ArtemisAPIGateway {
 
     public static void main(String[] args) {
-        SpringApplication.run(MultiplServicesConsulApplication.class, args);
+        SpringApplication.run(ArtemisAPIGateway.class, args);
     }
 
 }
