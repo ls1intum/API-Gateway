7e1a55b9a79bb052a0428ecab889051b39684f6d
Ole Vester <ole.vester@check24.de>
2025-02-17 13:29:22 +0100

    remove http3 bean

--- api-gateway/src/main/java/de/example/gateway/LoadBalancerConfig.java
+++ api-gateway/src/main/java/de/example/gateway/LoadBalancerConfig.java
@@ -1,15 +1,11 @@
 package de.example.gateway;
 
-import org.springframework.cloud.gateway.config.HttpClientCustomizer;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.loadbalancer.core.ReactorServiceInstanceLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.netty.http.HttpProtocol;
-
-import java.time.Duration;
 
 import static de.example.gateway.Constants.ARTEMIS_SERVICE_ID;
 import static de.example.gateway.Constants.ARTEMIS_SERVICE_NAME;
@@ -27,15 +23,5 @@ public class LoadBalancerConfig {
                 factory.getLazyProvider(ARTEMIS_SERVICE_ID, ServiceInstanceListSupplier.class).getObject(),
                 profilePathStore
         );
-    }
-
-    @Bean
-    HttpClientCustomizer http3HttpClientCustomizer() {
-        return httpClient ->
-            httpClient
-                .protocol(HttpProtocol.HTTP3)
-                .http3Settings(spec -> spec.idleTimeout(Duration.ofSeconds(5))
-                        .maxData(10_000_000)
-                        .maxStreamDataBidirectionalLocal(1_000_000));
     }
 }
